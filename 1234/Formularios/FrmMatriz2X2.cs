48e37377f9fe37d1fccb01f30ea776c0dc9e09e7
kateV1013 <kvurbina@uamv.edu.ni>
2025-10-17 00:30:59 -0600

    Matrices nuevooooooooooo

--- 1234/Formularios/FrmMatriz2X2.cs
+++ 1234/Formularios/FrmMatriz2X2.cs
@@ -16,5 +16,54 @@ namespace _1234.Formularios
         {
             InitializeComponent();
         }
+
+        int[,] matriz = new int[2, 2];
+        int fila = 0 , columna = 0;
+
+
+        private void FrmMatriz2X2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNum_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter) return;
+            if (!int.TryParse(tbNum.Text, out int num))
+            {
+                MessageBox.Show("Si no sabe escribir numeros, mejor dediquese a otra cosa", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+            }
+            matriz[fila, columna] = num;
+            tbNum.Clear();
+            lblMatriz2.Text = Mostrar();
+            columna++;
+            if (columna == 2)
+            {
+                columna = 0;
+                fila++;
+            }
+            if (fila == 2)
+            {
+                MessageBox.Show("Matriz llena", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbNum.Enabled = false;
+            }
+            tbNum.Focus();
+            e.SuppressKeyPress = true;
+
+        }
+        private string Mostrar()
+        {
+           StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Matriz 2x2");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    sb.Append($"{matriz[i, j], 4}");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
     }
 }
--- 1234/Formularios/FrmMatriz3X3cs.cs
+++ 1234/Formularios/FrmMatriz3X3cs.cs
@@ -16,5 +16,45 @@ namespace _1234.Formularios
         {
             InitializeComponent();
         }
+        int[,] matriz = new int[3, 3];
+        int fila = 0, columna = 0;
+        private void tbNum_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter) return;
+            if (!int.TryParse(tbNum.Text, out int num))
+            {
+                MessageBox.Show("Si no sabe escribir numeros, mejor dediquese a otra cosa", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); return;
+            }
+            matriz[fila, columna] = num;
+            tbNum.Clear();
+            lblMatriz.Text = Mostrar();
+            columna++;
+            if (columna == 3)
+            {
+                columna = 0;
+                fila++;
+            }
+            if (fila == 3)
+            {
+                MessageBox.Show("Matriz llena", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbNum.Enabled = false;
+            }
+            tbNum.Focus();
+            e.SuppressKeyPress = true;
+        }
+        private string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Matriz 3x3");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sb.Append($"{matriz[i, j], 4}");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
     }
 }
